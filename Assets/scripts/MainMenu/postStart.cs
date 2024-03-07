c1c0814d185eefb108ec94aa203f5d6f64b7cb84
prjktalix <briancamilo0720@gmail.com>
2024-03-07 01:39:28 -0800

    Update postStart.cs

--- Assets/scripts/MainMenu/postStart.cs
+++ Assets/scripts/MainMenu/postStart.cs
@@ -43,7 +43,7 @@ public class postStart : MonoBehaviour
     public void levelSelect()
     {
         levelSelection.SetActive(true);
-        isStart = true;
+        isStart = true; 
         GameMenu.SetActive(false);
     }
 
