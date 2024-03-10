e1f7a19728b568ab7fbeddacdf07d92ee36949bc
abali13 <abali1@ucsc.edu>
2024-03-09 21:31:40 -0800

    Working Localization

--- Assets/scripts/MainMenu/openingLanguage.cs
+++ Assets/scripts/MainMenu/openingLanguage.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Localization.Settings;
 
 public class openingLanguage : MonoBehaviour
 {
@@ -11,21 +12,25 @@ public class openingLanguage : MonoBehaviour
 
     public void english()
     {
-
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
         SceneManager.LoadScene("postStartScene");
+        //SetLocale(0);
         Debug.Log("english working");
     }
 
     public void french()
     {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
         SceneManager.LoadScene("postStartScene");
+        //SetLocale(1);
         Debug.Log("french working");
     }
 
     public void spanish()
     {
-        
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[3];
         SceneManager.LoadScene("postStartScene");
+        //SetLocale(2);
         Debug.Log("spanish working");
     }
 }
