74c78091414667718021a3deb44de18d1af43e01
ardlee <ardlee@ucsc.edu>
2024-04-20 20:34:41 -0700

    timer put into leaderboard score

--- Assets/scripts/Leaderboard/ScoreManager.cs
+++ Assets/scripts/Leaderboard/ScoreManager.cs
@@ -1,16 +1,35 @@
 
+//using UnityEngine;
+//using TMPro;
+//using UnityEngine.Events;
+//public class ScoreManager : MonoBehaviour
+//{
+//    [SerializeField] private TextMeshProUGUI inputScore;
+//    //[SerializeField] TMP_InputField inputName;
+//    [SerializeField] private TextMeshProUGUI inputName;
+
+//    public UnityEvent<string, int> submitScoreEvent;
+//    public void SubmitScore()
+//    {
+//        submitScoreEvent.Invoke(inputName.text, int.Parse(inputScore.text));
+//    }
+//}
+
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
+
 public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI inputScore;
-    //[SerializeField] TMP_InputField inputName;
-    [SerializeField] private TextMeshProUGUI inputName;
+    public Timer timerScript; // Reference to the Timer script
+    public TextMeshProUGUI inputScore;
+    public TextMeshProUGUI inputName;
 
     public UnityEvent<string, int> submitScoreEvent;
+
     public void SubmitScore()
     {
-        submitScoreEvent.Invoke(inputName.text, int.Parse(inputScore.text));
+        float timerValue = timerScript.TimerValue;
+        submitScoreEvent.Invoke(inputName.text, (int)timerValue); // Assuming you want to use the timer value as an integer score
     }
 }
