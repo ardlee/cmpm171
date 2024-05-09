d348a4754439bbba1c1f51e82c049c9ef2c75099
ardlee <ardlee@ucsc.edu>
2024-05-08 21:13:56 -0700

    Leaderboard

--- Assets/scripts/Leaderboard/ScoreManager.cs
+++ Assets/scripts/Leaderboard/ScoreManager.cs
@@ -1,35 +1,35 @@
 
-//using UnityEngine;
-//using TMPro;
-//using UnityEngine.Events;
-//public class ScoreManager : MonoBehaviour
-//{
-//    [SerializeField] private TextMeshProUGUI inputScore;
-//    //[SerializeField] TMP_InputField inputName;
-//    [SerializeField] private TextMeshProUGUI inputName;
-
-//    public UnityEvent<string, int> submitScoreEvent;
-//    public void SubmitScore()
-//    {
-//        submitScoreEvent.Invoke(inputName.text, int.Parse(inputScore.text));
-//    }
-//}
-
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
-
 public class ScoreManager : MonoBehaviour
 {
-    public Timer timerScript; // Reference to the Timer script
-    public TextMeshProUGUI inputScore;
-    public TextMeshProUGUI inputName;
+    [SerializeField] private TextMeshProUGUI inputScore;
+    //[SerializeField] TMP_InputField inputName;
+    [SerializeField] private TextMeshProUGUI inputName;
 
     public UnityEvent<string, int> submitScoreEvent;
-
     public void SubmitScore()
     {
-        float timerValue = timerScript.TimerValue;
-        submitScoreEvent.Invoke(inputName.text, (int)timerValue); // Assuming you want to use the timer value as an integer score
+        submitScoreEvent.Invoke(inputName.text, int.Parse(inputScore.text));
     }
 }
+
+//using UnityEngine;
+//using TMPro;
+//using UnityEngine.Events;
+
+//public class ScoreManager : MonoBehaviour
+//{
+//    public Timer timerScript; // Reference to the Timer script
+//    public TextMeshProUGUI inputScore;
+//    public TextMeshProUGUI inputName;
+
+//    public UnityEvent<string, int> submitScoreEvent;
+
+//    public void SubmitScore()
+//    {
+//        float timerValue = timerScript.TimerValue;
+//        submitScoreEvent.Invoke(inputName.text, (int)timerValue);
+//    }
+//}
